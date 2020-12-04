5da55bfa63bb60fa854e3342739c5b865839f5cc
Tomasz Darmetko <isinlor@gmail.com>
2020-12-04 10:31:44 +0100

    Add robot rotation documentation

--- src/Webots/WebotsNode.java
+++ src/Webots/WebotsNode.java
@@ -15,10 +15,22 @@ public class WebotsNode {
         return node;
     }
 
+    /**
+     * @see Vector#rotationVector
+     * @link https://cyberbotics.com/doc/reference/transform#field-summary
+     * @link https://cyberbotics.com/doc/reference/supervisor?tab-language=java#wb_supervisor_node_get_field
+     * @link https://cyberbotics.com/doc/reference/supervisor?tab-language=java#wb_supervisor_field_get_sf_rotation
+     */
     public Vector getRotation() {
         return new Vector(node.getField("rotation").getSFRotation());
     }
 
+    /**
+     * @see Vector#rotationVector
+     * @link https://cyberbotics.com/doc/reference/transform#field-summary
+     * @link https://cyberbotics.com/doc/reference/supervisor?tab-language=java#wb_supervisor_node_get_field
+     * @link https://cyberbotics.com/doc/reference/supervisor?tab-language=java#wb_supervisor_field_set_sf_rotation
+     */
     public void setRotation(Vector rotationVector) {
         node.getField("rotation").setSFRotation(rotationVector.getDoubleArray());
     }
