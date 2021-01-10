c026d6ecc7b8e2ac54d8db138160c828e9cca21d
Tomasz Darmetko <isinlor@gmail.com>
2021-01-10 01:39:05 +0100

    Add one more test of structure.

--- src/EDMO/ExampleStructure.java
+++ src/EDMO/ExampleStructure.java
@@ -26,6 +26,20 @@ public class ExampleStructure {
         }
 
         try {
+            Module moduleA2 = new Module();
+            Module moduleL2 = new Module();
+            Module moduleR2 = new Module();
+            new Structure(
+                new Connection(moduleA, moduleL, Connection.Connector.L, Connection.Connector.B, Connection.Orientation.H),
+                new Connection(moduleA, moduleR, Connection.Connector.R, Connection.Connector.B, Connection.Orientation.H),
+                new Connection(moduleA2, moduleL2, Connection.Connector.L, Connection.Connector.B, Connection.Orientation.H),
+                new Connection(moduleA2, moduleR2, Connection.Connector.R, Connection.Connector.B, Connection.Orientation.H)
+            );
+        } catch (Exception e) {
+            System.out.println("Expected exception: " + e.getMessage());
+        }
+
+        try {
             new Structure(
                 new Connection(moduleA, moduleL, Connection.Connector.T, Connection.Connector.B, Connection.Orientation.H),
                 new Connection(moduleA, moduleR, Connection.Connector.T, Connection.Connector.B, Connection.Orientation.H)
